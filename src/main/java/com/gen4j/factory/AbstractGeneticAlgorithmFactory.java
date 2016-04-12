33e4656a11a693b9cbd202e03abaf78a4c4f1894
RenanSFreitas <renansouza92@hotmail.com>
2016-04-11 23:58:13 -0300

    removes fitness cache for now

--- src/main/java/com/gen4j/factory/AbstractGeneticAlgorithmFactory.java
+++ src/main/java/com/gen4j/factory/AbstractGeneticAlgorithmFactory.java
@@ -2,25 +2,11 @@ package com.gen4j.factory;
 
 import java.util.Optional;
 
-import com.gen4j.fitness.FitnessCache;
-import com.gen4j.fitness.FitnessFunction;
 import com.gen4j.genotype.Genotype;
 import com.gen4j.population.generator.RandomGenotypeGenerator;
 
 public abstract class AbstractGeneticAlgorithmFactory<G extends Genotype, V, P>
         implements GeneticAlgorithmFactory<G, V, P> {
-
-    private FitnessCache<G> fitnessCache;
-
-    protected abstract FitnessFunction<G> internalFitnessFunction();
-
-    @Override
-    public FitnessFunction<G> fitnessFunction() {
-        if (fitnessCache == null) {
-            fitnessCache = new FitnessCache<>(internalFitnessFunction());
-        }
-        return fitnessCache;
-    }
 
     @Override
     public Optional<RandomGenotypeGenerator<G>> genotypeGenerator() {
