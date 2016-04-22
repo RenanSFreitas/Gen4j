bcf87fc1b439cc1f241a7e1f66c37cb01036c4df
RenanSFreitas <renan.souza.de.freitas@ericsson.com>
2016-04-22 10:59:29 -0300

    Criteria fix

--- src/main/java/com/gen4j/population/Criteria.java
+++ src/main/java/com/gen4j/population/Criteria.java
@@ -17,7 +17,7 @@ public interface Criteria<G extends Chromosome> {
     static <C extends Chromosome> Criteria<C> noFittestChanges(final int generations, final double minimumImprovement) {
         return new Criteria<C>() {
             private int lastFitnessGeneration = 0;
-            private double lastFitness;
+			private double lastFitness = Integer.MIN_VALUE;
             @Override
             public boolean apply(final Population<C> population, final int generation) {
                 if (generation - lastFitnessGeneration < generations) {
