a7dc3309d6993a8d4dedae7c75e8ac2ca3fb6131
RenanSFreitas <renansouza92@hotmail.com>
2016-04-22 00:43:26 -0300

    BitChromosomeFactory not abstract

--- src/main/java/com/gen4j/factory/bit/BitChromosomeFactory.java
+++ src/main/java/com/gen4j/factory/bit/BitChromosomeFactory.java
@@ -8,15 +8,22 @@ import com.gen4j.chromosome.ChromosomeCoder;
 import com.gen4j.chromosome.bit.BitChromosome;
 import com.gen4j.chromosome.bit.BitChromosomeCoder;
 import com.gen4j.factory.GenericGeneticAlgorithmFactory;
+import com.gen4j.fitness.FitnessFunction;
+import com.gen4j.population.Criteria;
 import com.gen4j.population.generator.ChromosomeGenerator;
 import com.gen4j.population.generator.RandomBitChromosomeGenerator;
 
-public abstract class BitChromosomeFactory extends GenericGeneticAlgorithmFactory<BitChromosome> {
+public class BitChromosomeFactory extends GenericGeneticAlgorithmFactory<BitChromosome> {
 
     private final BitChromosomeCoder coder;
+    private final FitnessFunction<BitChromosome> fitnessFunction;
+    private final Criteria<BitChromosome> stopCriteria;
 
-    public BitChromosomeFactory(final BitChromosomeCoder coder) {
+    public BitChromosomeFactory(final BitChromosomeCoder coder, final FitnessFunction<BitChromosome> fitnessFunction,
+            final Criteria<BitChromosome> stopCriteria) {
         this.coder = requireNonNull(coder);
+        this.fitnessFunction = requireNonNull(fitnessFunction);
+        this.stopCriteria = requireNonNull(stopCriteria);
     }
 
     @Override
@@ -27,6 +34,16 @@ public abstract class BitChromosomeFactory extends GenericGeneticAlgorithmFactor
     @Override
     public Optional<ChromosomeGenerator<BitChromosome>> chromosomeGenerator() {
         return Optional.of(new RandomBitChromosomeGenerator());
+    }
+
+    @Override
+    public FitnessFunction<BitChromosome> fitnessFunction() {
+        return fitnessFunction;
+    }
+
+    @Override
+    public Criteria<BitChromosome> stopCriteria() {
+        return stopCriteria;
     }
 
 }
