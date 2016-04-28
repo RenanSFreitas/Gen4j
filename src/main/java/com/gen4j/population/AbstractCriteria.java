b2d99531c4bbd72610bd37b02cde81708c143cdb
RenanSFreitas <renansouza92@hotmail.com>
2016-04-27 21:45:31 -0300

    Convergence test based on avg and max

--- src/main/java/com/gen4j/population/AbstractCriteria.java
+++ src/main/java/com/gen4j/population/AbstractCriteria.java
@@ -1,15 +1,18 @@
 package com.gen4j.population;
 
 import com.gen4j.chromosome.Chromosome;
+import com.google.common.math.DoubleMath;
 
 public abstract class AbstractCriteria<C extends Chromosome> implements Criteria<C> {
+
+    private static final double TOLERANCE = 0.000001;
 
     @Override
     public final boolean apply(final Population<C> population, final int generation) {
 
         final PopulationStatistics statistics = population.statistics();
 
-        if (statistics.maxFitness() == statistics.minFitness()) {
+        if (DoubleMath.fuzzyEquals(statistics.meanFitness(), statistics.maxFitness(), TOLERANCE)) {
             return true;
         }
 
