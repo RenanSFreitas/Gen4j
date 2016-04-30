fbcb29993dd13c896509bdcc306f0523e099d18c
RenanSFreitas <renansouza92@hotmail.com>
2016-04-30 14:32:20 -0300

    Refactors impl to adequate to literature

--- src/main/java/com/gen4j/generation/replacement/StandardGenerationReplacer.java
+++ src/main/java/com/gen4j/generation/replacement/StandardGenerationReplacer.java
@@ -1,17 +1,15 @@
 package com.gen4j.generation.replacement;
 
-import static com.google.common.collect.Iterables.concat;
-import static com.google.common.collect.Lists.newArrayList;
-import static com.google.common.collect.Sets.newTreeSet;
-
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
 
 import com.gen4j.chromosome.Chromosome;
+import com.gen4j.factory.GeneticAlgorithmFactory;
 import com.gen4j.population.Individual;
+import com.gen4j.population.Population;
 import com.gen4j.population.PopulationBuilder;
+import com.google.common.collect.Multiset.Entry;
+import com.google.common.collect.TreeMultiset;
 
 public enum StandardGenerationReplacer {
 
@@ -24,32 +22,54 @@ public enum StandardGenerationReplacer {
     STEADY_STATE {
         @Override
         public <C extends Chromosome> GenerationReplacer<C> get() {
-            return (generation, nextGeneration, f) -> {
-                final List<Individual<C>> exchanged = newArrayList(concat(generation.fitness(), nextGeneration.fitness()));
-                Collections.sort(exchanged);
-                final int exchangedSize = exchanged.size();
-                final int nextGenerationSize = nextGeneration.size();
-                return PopulationBuilder.of(f).size(nextGenerationSize)
-                        .initialChromosomes(exchanged.subList(exchangedSize - nextGenerationSize, exchangedSize))
-                        .build();
+            return (parentsGeneration, offspringGeneration, factory) -> {
+                return steadyStateReplacement(parentsGeneration, offspringGeneration, factory, true);
             };
         }
+
     },
     STEADY_STATE_NO_DUPLICATES {
         @Override
         public <C extends Chromosome> GenerationReplacer<C> get() {
-            return (generation, nextGeneration, factory) -> {
-                final List<Individual<C>> parents = generation.fitness();
-                final int parentSize = parents.size();
-                final List<Individual<C>> exchanged = new ArrayList<>(parentSize);
-                final Iterator<Individual<C>> iterator = newTreeSet(concat(parents, nextGeneration.fitness())).descendingIterator();
-                for (int counter = 0; counter < parentSize && iterator.hasNext(); counter++) {
-                    exchanged.add(iterator.next());
-                }
-                return PopulationBuilder.of(factory).size(parentSize).initialChromosomes(exchanged).build();
+            return (parentsGeneration, offspringGeneration, factory) -> {
+                return steadyStateReplacement(parentsGeneration, offspringGeneration, factory, false);
             };
         }
     };
 
     public abstract <C extends Chromosome> GenerationReplacer<C> get();
+
+    protected <C extends Chromosome> Population<C> steadyStateReplacement(final Population<C> parentsGeneration,
+            final Population<C> offspringGeneration, final GeneticAlgorithmFactory<C> factory,
+            final boolean allowDuplicates) {
+
+        final TreeMultiset<Individual<C>> parents = TreeMultiset.create();
+        parents.addAll(parentsGeneration.toList());
+
+        final List<Individual<C>> offsprings = offspringGeneration.toList();
+        final List<Individual<C>> survivalOffsprings = new ArrayList<>(offspringGeneration.size());
+        for (int i = 0; i < offsprings.size() && !parents.isEmpty(); i++) {
+            final Individual<C> worstParent = parents.firstEntry().getElement();
+            final Individual<C> offspring = offsprings.get(i);
+
+            if (offspring.fitness() > worstParent.fitness() && (allowDuplicates || !parents.contains(offspring))) {
+                parents.remove(worstParent);
+                survivalOffsprings.add(offspring);
+            }
+        }
+
+
+        final List<Individual<C>> nextGeneration = new ArrayList<>(parents.size());
+        for (final Entry<Individual<C>> parent : parents.entrySet()) {
+            for (int i = 0; i < parent.getCount(); i++) {
+                nextGeneration.add(parent.getElement());
+            }
+        }
+        nextGeneration.addAll(survivalOffsprings);
+
+        return PopulationBuilder.of(factory)
+                .size(nextGeneration.size())
+                .initialChromosomes(nextGeneration)
+                .build();
+    }
 }
