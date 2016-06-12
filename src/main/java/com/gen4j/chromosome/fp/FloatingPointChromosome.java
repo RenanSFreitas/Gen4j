bb70fed3b079cb68bde85d40eb9da7262cd92205
RenanSFreitas <renansouza92@hotmail.com>
2016-06-11 22:41:41 -0300

    equals and hashcode

--- src/main/java/com/gen4j/chromosome/fp/FloatingPointChromosome.java
+++ src/main/java/com/gen4j/chromosome/fp/FloatingPointChromosome.java
@@ -3,6 +3,9 @@ package com.gen4j.chromosome.fp;
 import static com.google.common.base.Preconditions.checkArgument;
 import static java.util.Objects.requireNonNull;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 import com.gen4j.chromosome.Chromosome;
 
 /**
@@ -25,6 +28,24 @@ public class FloatingPointChromosome implements Chromosome {
     @Override
     public int length() {
         return value.length;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (!(obj instanceof FloatingPointChromosome)) {
+            return false;
+        }
+
+        return Arrays.equals(value, ((FloatingPointChromosome) obj).value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
     }
 
 }
